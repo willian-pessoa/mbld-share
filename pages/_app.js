d1b4c2f0f5f09a82a4317d7cc46e2ae87d15b181
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-02-24 14:11:26 -0300

    title and ico add

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.scss'
 import Layout from "../components/Layout/Layout";
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>MBLD Share</title>
+        <link rel="icon" href="/assets/bld.ico" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
