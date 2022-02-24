07be9b134147b5094d384c0361bf485b3a04468f
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-02-24 17:47:43 -0300

    video change

--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,7 @@ import styles from "../styles/Login.module.scss";
 import Image from "next/image"
 import Router from 'next/router'
 
-import {client} from "../functions/client.js"
+import { client } from "../functions/client.js"
 
 export default function Login() {
 
@@ -13,7 +13,7 @@ export default function Login() {
         //console.log(response)
         localStorage.setItem("user", JSON.stringify(response.profileObj));
 
-        const {name, googleId, imageUrl} = response.profileObj;
+        const { name, googleId, imageUrl } = response.profileObj;
 
         const doc = {
             _id: googleId,
@@ -24,28 +24,28 @@ export default function Login() {
         }
 
         client.createIfNotExists(doc)
-        .then(() => {
-            Router.push("/")
-        })
+            .then(() => {
+                Router.push("/")
+            })
     }
 
     return <div className={styles.login_container}>
-        <video src="assets/mbld2.mp4" type="video/mp4" loop controls={false} autoPlay muted />
+        <video src="assets/mbld3.mp4" type="video/mp4" loop controls={false} autoPlay muted />
         <div className={styles.login_bg}>
             <div className={styles.login}>
                 <Image priority src="/assets/logo.png" width="150" height="51" alt="logo" />
-                    <GoogleLogin
-                        clientId={`${process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_ID}`}
-                        render={renderProps => (
-                            <button onClick={renderProps.onClick} disabled={renderProps.disabled}>
-                                <FcGoogle style={{marginRight: "10px"}}/>
-                                Sing in with Google</button>
-                        )}
-                        buttonText="Login"
-                        onSuccess={responseGoogle}
-                        onFailure={responseGoogle}
-                        cookiePolicy={'single_host_origin'}
-                    />
+                <GoogleLogin
+                    clientId={`${process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_ID}`}
+                    render={renderProps => (
+                        <button onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                            <FcGoogle style={{ marginRight: "10px" }} />
+                            Sing in with Google</button>
+                    )}
+                    buttonText="Login"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                />
             </div>
         </div>
     </div>;
